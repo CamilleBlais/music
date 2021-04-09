4e4cf88b73d5e5a196d1f0910b3e6110947f069a
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2021-04-09 12:37:08 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -1,12 +1,22 @@
 
-//variables
+//////////////variables
 
 //var TikY = 0;
 var i = 0;
+var r = 0;
+var g = 0;
+var b = 0;
+
+
+r = random(0,255);
+g = random(0,255);
+b = random(0,255);
+
+////////////////////////////
 
 
 function setup() {
-createCanvas(windowWidth, 3500);
+createCanvas(windowWidth, 3700);
    background(121,205,191,100);
    //frameRate(6);
   }
@@ -18,10 +28,24 @@ createCanvas(windowWidth, 3500);
 
 function draw() {
   noStroke();
-  fill(100,54,200);
+  fill(r,g,b);
   rect(windowWidth/2 -200 , 10, 400, 550, 20);
    
-  fill(150,154,100);
+  fill(r,g,b);
   rect(windowWidth/2 -200 , 610, 400, 550, 20);
   
+  fill(r,g,b);
+  rect(windowWidth/2 -200 , 1210, 400, 550, 20);
+   
+  fill(r,g,b);
+  rect(windowWidth/2 -200 , 1810, 400, 550, 20);
+   
+  fill(r,g,b);
+  rect(windowWidth/2 -200 , 2410, 400, 550, 20);
+   
+  fill(r,g,b);
+  rect(windowWidth/2 -200 , 3010, 400, 550, 20);
+   
+  fill(r,g,b);
+  rect(windowWidth/2 -200 , 3610, 400, 550, 20);
   }
