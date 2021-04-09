d303048e513aa0caf4fb3cf682b724bfe936e06c
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2021-04-09 12:27:53 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -6,21 +6,17 @@ var i = 0;
 
 
 function setup() {
-createCanvas(500, 3500);
+createCanvas(windowWidth, 3500);
    background(121,205,191,100);
    //frameRate(6);
   }
 
 
-///Définitions de Fonctions 
-
-//function FakeTiktok(windowWidth/2, TikY){
-//}
 
 
 
 
 function draw() {
-  ellipse(50,2500,50,50);
+  rect(windowWidth/2, 10, 55, 55, 20);
   
   }
