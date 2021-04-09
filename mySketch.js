fef4672d1929f0d8e56163ef990fcad5e0c7d669
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2021-04-09 12:32:25 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -17,6 +17,11 @@ createCanvas(windowWidth, 3500);
 
 
 function draw() {
+  noStroke();
+  fill(100,54,200);
   rect(windowWidth/2 -200 , 10, 400, 550, 20);
+   
+  fill(150,154,100);
+  rect(windowWidth/2 -200 , 610, 400, 550, 20);
   
   }
