2a04fd11e7ba9c30724e04ecb6d9bb9910bab035
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2021-04-14 12:43:12 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -2,7 +2,7 @@
 //////////////variables
 
 var i = 0;
-var song;
+//var song;
 
 //var drops = [];
 
@@ -15,7 +15,7 @@ var song;
 function setup(){
 createCanvas(windowWidth, 9040);
 
-song = loadSound("SON01.mp3");  
+//song = loadSound("SON01.mp3");  
 
    
 
@@ -29,9 +29,9 @@ song = loadSound("SON01.mp3");
 
 //////////////////////////////////////fonction loaded
 
-function loaded() {
-song.play();
-}
+//function loaded() {
+//song.play();
+//}
 
 
 
