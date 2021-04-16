8b15bf2ea5206194bdc496e759c47e0d9e5b5202
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2021-04-16 18:06:32 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -197,7 +197,16 @@ rect1(rectX,8220);
 } //end of draw
 
 
-
+	function mousePressed(){
+	if(mouseX > rectX && mouseX < rectX+660 && mouseY > 20 && mouseY < 820){
+	son1.loop();
+	bouton = true;
+        }}
+	function mousePressed(){
+	if(mouseX > rectX && mouseX < rectX+660 && mouseY > 20 && mouseY < 820){
+	son1.pause();
+	bouton = false;
+        }}
 
 
 
