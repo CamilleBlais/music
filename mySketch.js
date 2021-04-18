ebe00071389116da182a286ed5fecc946d67a837
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2021-04-18 18:54:44 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -74,11 +74,22 @@ for (var i = 0; i < 7000; i++) {    //light rain
    	drops[i] = new Drop();
   	}
 
-		
-
-	
-
 }
+
+////////////////////////////////////////////////////////////////////
+
+function VideoAudio(nameAudio,boovar, positionY, positionYY){
+function mousePressed(){
+if(mouseX > rectX && mouseX < rectX+660 && mouseY > positionY && mouseY < positionYY){
+if(boovar){
+nameAudio.loop();
+boovar = false;
+}else{
+nameAudio.pause();
+boovar=true;
+}
+}
+}}
 
 
 ///////////////////////////////////////////////////////// fonction draw
@@ -246,18 +257,7 @@ VideoAudio(son1,premierIsPlaying,20,820);
 /////////////////////////////////////////////////////////////////////////////definitions
 
 
-function VideoAudio(nameAudio,boovar, positionY, positionYY){
-function mousePressed(){
-if(mouseX > rectX && mouseX < rectX+660 && mouseY > positionY && mouseY < positionYY){
-if(boovar){
-nameAudio.loop();
-boovar = false;
-}else{
-nameAudio.pause();
-boovar=true;
-}
-}
-}}
+
 
 
 
